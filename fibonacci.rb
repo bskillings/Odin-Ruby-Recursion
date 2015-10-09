f40f0d0e7228e8a52d29552fb44eb2673954cfdc
Becky Skillings <beckyskillings@gmail.com>
2015-10-09 11:00:14 -0600

    formal recursion project

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,10 +1,23 @@
-def fibonacci(n)
-	return 0 if n == 0
-	return 1 if n == 1
-	return fibonacci(n-1) + fibonacci(n-2)
+#iteration: works
+def fibs(n)
+	fib_array = []
+	0.upto(n) do |i|
+		if i < 2
+			fib_array[i] = i
+		else
+			fib_array[i] = (fib_array[i-2] + fib_array[i-1])
+		end
+	end
+	puts fib_array
 end
 
-puts fibonacci(2)
-puts fibonacci(7)
 
-#oh yeah, worked on first try
+ #this currently returns only the number in the given slot
+def fibs_rec(n)
+	n < 2 ? n : fibs_rec(n-1) + fibs_rec(n-2)
+end
+
+puts fibs(5)
+puts fibs_rec(5)
+puts fibs(6)
+puts fibs_rec(6)
