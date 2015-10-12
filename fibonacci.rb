7265ba791176ebf0d91c3c453b2c99c42f895e9a
Becky Skillings <beckyskillings@gmail.com>
2015-10-12 10:53:46 -0600

    tidied up recursive fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,25 +1,32 @@
-#iteration: works, prints out whole list
-def fibs(n)
-	fib_array = []
-	0.upto(n) do |i|
-		if i < 2
-			fib_array[i] = i
-		else
-			fib_array[i] = (fib_array[i-2] + fib_array[i-1])
-		end
+class Fibonacci
+
+	def initialize(n)
+		puts "Iteration:"
+		puts fibs(n)
+		puts "Recursion:"
+		(n+1).times {|i| puts fibs_rec(i)}
 	end
-	puts fib_array
+
+	#iteration: works, prints out whole list
+	def fibs(n)
+		fib_array = []
+		0.upto(n) do |i|
+			if i < 2
+				fib_array[i] = i
+			else
+				fib_array[i] = (fib_array[i-2] + fib_array[i-1])
+			end
+		end
+		puts fib_array
+	end
+
+
+	 #this currently returns only the number in the given slot
+	def fibs_rec(n)
+		n < 2 ? n : fibs_rec(n-1) + fibs_rec(n-2)
+	end
 end
 
+fibs5 = Fibonacci.new(5)
+fibs6 = Fibonacci.new(6)
 
- #this currently returns only the number in the given slot
-def fibs_rec(n)
-	n < 2 ? n : fibs_rec(n-1) + fibs_rec(n-2)
-end
-
-puts fibs(5)
-0.upto(5) {|i| puts fibs_rec(i)}
-puts fibs(6)
-0.upto(6) {|i| puts fibs_rec(i)}
-
-#I am indebted to Marina Sergeyeva for the idea to just put the upto outside the darn fibs_rec
